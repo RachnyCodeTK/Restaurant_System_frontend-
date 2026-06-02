fbb41c3fa6be328bba3486f4a091cb57bf9287d8
Lout Rachny <rachnyb.l@gmail.com>
2026-06-02 11:35:34 +0700

    fix: update login redirect logic and styled UI

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
-export default defineConfig({
   build: {
-    chunkSizeWarningLimit: 1000 // បង្កើនពី 500KB ទៅ 1000KB (ឬតាមការកំណត់របស់អ្នក)
+    chunkSizeWarningLimit: 1000 // default: 500
   }
 })
